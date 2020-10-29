748e553200a05b0ed5c775a47909b7ecfef653f8
jmpmato <github@olameu.com>
2020-10-29 18:19:29 +0100

    Posibilidad de cambiar textos

--- coreModules/common/classes/view/templates/js/ckAcepto.js
+++ coreModules/common/classes/view/templates/js/ckAcepto.js
@@ -5,11 +5,20 @@ var cogumelo = cogumelo || {};
 cogumelo.ckAcepto = {
   base: 'ck-acepto-eu-law', // No cambiar
   version: '2010',
-  link: '/cookies',
-  txtAceptar: 'Aceptar',
   keyGA: false
 };
+
 cogumelo.ckAcepto.name = cogumelo.ckAcepto.base + '-' + cogumelo.ckAcepto.version;
+
+// Para adaptar y traducir
+cogumelo.ckAcepto.link = '/cookies';
+cogumelo.ckAcepto.txtAceptar = 'Aceptar';
+cogumelo.ckAcepto.msg = 'Utilizamos cookies propias y de terceros para analizar sus preferencias y hábitos de navegación. ' +
+  'Puede cambiar la configuración, seleccionar de manera específica qué cookies consiente y cuáles no y obtener más información. ' +
+  'Puede cambiar la configuración u obtener más información ' +
+  '<a style="font-weight:700;color:#CCC;text-decoration:underline;" href="' + cogumelo.ckAcepto.link + '">aquí</a>. ' +
+  'Pulse el botón "' + cogumelo.ckAcepto.txtAceptar + '" para consentir el uso de todas las cookies.';
+
 
 
 // Google Analytics function
@@ -84,10 +93,6 @@ cogumelo.ckAcepto.showPanel = function showPanel() {
 
   if( document.getElementById( this.base + '-panel' ) === null ) {
 
-    var ckBlockMsg = 'Utilizamos cookies propias y de terceros para analizar sus preferencias y hábitos de navegación.' +
-      'Puede cambiar la configuración, seleccionar de manera específica qué cookies consiente y cuáles no y obtener más información. Puedes cambiar la configuración u obtener más información ' +
-      '<a style="font-weight:700;color:#CCC;text-decoration:underline;" href="' + this.link + '">aquí</a>. ' +
-      'Pulsa el botón "' + this.txtAceptar + '" para consentir el uso de todas las cookies.';
     var ckBlockStyle = 'position:fixed;bottom:0;width:100%;padding:25px;' +
       'color:#CCC;background-color:rgba(22,22,22,0.9);font-size:15px;line-height:26px;' +
       'border:2px solid #EEE;z-index:99999;';
@@ -101,7 +106,7 @@ cogumelo.ckAcepto.showPanel = function showPanel() {
       '<div class="ckBlock" style="margin:0 auto;padding:15px;">' +
         '<span class="ckBlockSub" style="margin:0;padding:0;">' +
           '<a class="ckButton acepto" style="' + ckBotStyle + '">' + this.txtAceptar + '</a>' +
-          '<span class="ckBlockSub">' + ckBlockMsg + '</span>' +
+          '<span class="ckBlockSub">' + cogumelo.ckAcepto.msg + '</span>' +
         '</span>' +
       '</div>'
     ;
