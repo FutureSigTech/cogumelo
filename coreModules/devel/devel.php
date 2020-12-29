4b80838b77634ca69ce0dbfec6b79d629c829ac7
jmpmato <github@olameu.com>
2020-12-29 12:45:13 +0100

    Pasamos a kint 3.3 para PHP actualizado

--- coreModules/devel/devel.php
+++ coreModules/devel/devel.php
@@ -9,49 +9,47 @@ class devel extends Module {
   public $name = "devel";
   public $version = 1.0;
   public $dependences = array(
-   // BOWER
+    // BOWER
+    array(
+      "id" => "jquery",
+      "params" => array("jQuery#2.2"),
+      "installer" => "bower",
+      "includes" => array("dist/jquery.min.js")
+    ),
+    array(
+      "id" => "bootstrap",
+      "params" => array("bootstrap#v3.4"),
+      "installer" => "bower",
+      "includes" => array("dist/js/bootstrap.min.js")
+    ),
+    array(
+      "id" =>"d3",
+      "params" => array("d3#v3.0.6"),
+      "installer" => "bower",
+      "includes" => array("d3.min.js")
+    ),
+    array(
+      "id" =>"webcola",
+      "params" => array("webcola#3.0.0"),
+      "installer" => "bower",
+      "includes" => array("WebCola/cola.v3.min.js")
+    ),
 
-   array(
-     "id" => "jquery",
-     "params" => array("jQuery#2.2"),
-     "installer" => "bower",
-     "includes" => array("dist/jquery.min.js")
-   ),
-
-   array(
-    "id" => "bootstrap",
-    "params" => array("bootstrap#v3.4"),
-    "installer" => "bower",
-    "includes" => array("dist/js/bootstrap.min.js")
-   ),
-   array(
-     "id" =>"d3",
-     "params" => array("d3#v3.0.6"),
-     "installer" => "bower",
-     "includes" => array("d3.min.js")
-   ),
-   array(
-     "id" =>"webcola",
-     "params" => array("webcola#3.0.0"),
-     "installer" => "bower",
-     "includes" => array("WebCola/cola.v3.min.js")
-   ),
-
-   // COMPOSER
-   array(
-     "id" => "kint",
-     //"params" => array("raveren/kint","1.0.*@dev"),
-     "params" => array("kint-php/kint","1.0.*@dev"),
-     "installer" => "composer",
-     "includes" => array("Kint.class.php")
-   ),
-   array(
-     "id" => "sqlFormatter",
-     "params" => array("jdorn/sql-formatter", "1.3.*@dev"),
-     "installer" => "composer",
-     "includes" => array("lib/SqlFormatter.php")
-   )
-
+    // COMPOSER
+    [
+      'id' => 'kint',
+      // 'params' => array('raveren/kint','1.0.*@dev'),
+      // 'params' => array('kint-php/kint','1.0.*@dev'), // NO soporta PHP 7.4
+      'params' => [ 'kint-php/kint', '3.3' ],
+      'installer' => 'composer',
+      'includes' => [ 'Kint.class.php' ]
+    ],
+    array(
+      "id" => "sqlFormatter",
+      "params" => array("jdorn/sql-formatter", "1.3.*@dev"),
+      "installer" => "composer",
+      "includes" => array("lib/SqlFormatter.php")
+    ),
   );
 
   public $includesCommon = array(
@@ -64,15 +62,15 @@ class devel extends Module {
   );
 
   public function __construct() {
-/*
-    OLD URL's (now in moduledevelWebPanel)
-    $this->addUrlPatterns( '#^'.Cogumelo::getSetupValue( 'mod:devel:url' ).'$#', 'view:DevelView::main' );
-    $this->addUrlPatterns( '#^'.Cogumelo::getSetupValue( 'mod:devel:url' ).'/read_logs$#', 'view:DevelView::read_logs' );
-    $this->addUrlPatterns( '#^'.Cogumelo::getSetupValue( 'mod:devel:url' ).'/get_debugger#', 'view:DevelView::get_debugger' );
-    $this->addUrlPatterns( '#^'.Cogumelo::getSetupValue( 'mod:devel:url' ).'/get_sql_tables$#', 'view:DevelView::get_sql_tables' );
-    $this->addUrlPatterns( '#^'.Cogumelo::getSetupValue( 'mod:devel:url' ).'/phpinfo$#', 'view:DevelView::develPhpInfo' );
-    $this->addUrlPatterns( '#^'.Cogumelo::getSetupValue( 'mod:devel:url' ).'/phpinfo[/\#\?]+.*#', 'view:DevelView::develPhpInfo' );
-*/
+    /*
+      OLD URL's (now in moduledevelWebPanel)
+      $this->addUrlPatterns( '#^'.Cogumelo::getSetupValue( 'mod:devel:url' ).'$#', 'view:DevelView::main' );
+      $this->addUrlPatterns( '#^'.Cogumelo::getSetupValue( 'mod:devel:url' ).'/read_logs$#', 'view:DevelView::read_logs' );
+      $this->addUrlPatterns( '#^'.Cogumelo::getSetupValue( 'mod:devel:url' ).'/get_debugger#', 'view:DevelView::get_debugger' );
+      $this->addUrlPatterns( '#^'.Cogumelo::getSetupValue( 'mod:devel:url' ).'/get_sql_tables$#', 'view:DevelView::get_sql_tables' );
+      $this->addUrlPatterns( '#^'.Cogumelo::getSetupValue( 'mod:devel:url' ).'/phpinfo$#', 'view:DevelView::develPhpInfo' );
+      $this->addUrlPatterns( '#^'.Cogumelo::getSetupValue( 'mod:devel:url' ).'/phpinfo[/\#\?]+.*#', 'view:DevelView::develPhpInfo' );
+    */
     //$this->addUrlPatterns( '#^'.Cogumelo::getSetupValue( 'mod:devel:url' ).'/GC#', 'view:DevelView::runGarbageCollectors' );
     //$this->addUrlPatterns( '#^'.Cogumelo::getSetupValue( 'mod:devel:url' ).'/porto#', 'view:DevelView::develPorto' );
 
